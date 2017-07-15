bf2582a7119d3ec972cd5911be1aa48c31bb4cc1
parasbabbar <parasbabbar003@gmail.com>
2017-07-15 12:12:32 +0530

    ASSIGNMENT5

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(app.express.static(__dirname + '/public'));
 // require ("./test/app.js")(app);
 
 require('./assignment/app');
+require ("./test/app.js");
 
 var port = process.env.PORT || 3000;
 
