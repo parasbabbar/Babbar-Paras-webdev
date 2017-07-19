d45eb672de13d15fc5721a53ffdf1b96baad8ff1
parasbabbar <parasbabbar003@gmail.com>
2017-07-19 11:37:35 +0530

    ASSIGNMENT5

--- test/app.js
+++ test/app.js
@@ -1,21 +1,33 @@
-module.exports = function(app)
-{
+var app = require('../express');
+var q = require('q');
+
     app.get("/api/test", findAllMessages);
     app.post("/api/test", createMessage);
     app.delete("/api/test/:id", deleteMessage);
 
-    var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
-    if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-        var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-        var password = process.env.MLAB_PASSWORD_WEBDEV;
-        connectionString = 'mongodb://' + username + ':' + password;
-        connectionString += '@ds025459.mlab.com:25459/heroku_rw478k23'; // user yours
-    }
 
-    var mongoose = require("mongoose");
-    mongoose.connect(connectionString);
+    // if(process.env.MLAB_USERNAME) {
+    //     connectionString = process.env.MLAB_USERNAME + ":" +
+    //         process.env.MLAB_PASSWORD + "@" +
+    //         process.env.MLAB_HOST + ':' +
+    //         process.env.MLAB_PORT + '/' +
+    //         process.env.MLAB_APP_NAME;
+    // }
 
-    var TestSchema = mongoose.Schema({
+
+var connectionString = 'mongodb://127.0.0.1:27017/webdev'; // for local
+if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds025459.mlab.com:25459/heroku_rw478k23'; // user yours
+}
+
+var mongoose = require('mongoose');
+mongoose.connect(connectionString);
+mongoose.Promise = require('q').Promise;
+
+var TestSchema = mongoose.Schema({
         message: String
     });
 
@@ -59,4 +71,3 @@ module.exports = function(app)
                 }
             );
     }
-};
