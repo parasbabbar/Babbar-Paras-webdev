d79210773711cb55372af397c3c932e0723bbb5b
parasbabbar <parasbabbar003@gmail.com>
2017-08-12 16:40:47 +0530

    google config vars

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.use(cookieParser());
-app.use(session({ secret: "dnknekfnqeknqkfqenfkf",
+app.use(session({ secret: process.env.secret,
     resave: true,
     saveUninitialized: true
 }));
